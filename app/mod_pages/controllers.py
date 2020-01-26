c24743f26e58a02509d367dd591992a89d07da10
tiffanychalmersduong <tiffany.chalmers.duong@gmail.com>
2020-01-26 17:18:31 -0600

    Added Menu html sub pages and routes

--- app/mod_pages/controllers.py
+++ app/mod_pages/controllers.py
@@ -9,3 +9,23 @@ def welcome():
 @mod_pages.route('/menu')
 def render_main_menu():
     return render_template('pages/menu.html')
+
+@mod_pages.route('/menu/appetizers')
+def render_menu_appetizers():
+    return render_template('pages/menu_appetizers.html')
+
+@mod_pages.route('/menu/soups_salads')
+def render_menu_soups_salads():
+    return render_template('pages/menu_soups_salads.html')
+
+@mod_pages.route('/menu/signature_dishes')
+def render_menu_main_dishes():
+    return render_template('pages/menu_main_dishes.html')
+
+@mod_pages.route('/menu/desserts')
+def render_menu_desserts():
+    return render_template('pages/menu_desserts.html')
+
+@mod_pages.route('/menu/drinks')
+def render_menu_drinks():
+    return render_template('pages/menu_drinks.html')

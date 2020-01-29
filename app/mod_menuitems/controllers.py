3cf82103989e704d7ce53e7a9f21b5e104f559f4
Brody Hoskins <brody@brody.digital>
2020-01-28 18:00:40 -0800

    Move menu pages to mod_menuitems

--- app/mod_menuitems/controllers.py
+++ app/mod_menuitems/controllers.py
@@ -20,6 +20,36 @@ def index():
     menuitems = Menuitem.query.all()
     return render_template('menuitems/index.html', menuitems = menuitems)
 
+@mod_menuitems.route('/menu/appetizers')
+def render_menu_appetizers():
+    # Todo: Select menuitems belonging to category
+    menuitems = Menuitem.query.all()
+    return render_template('pages/menu-appetizers.html', menuitems = menuitems)
+
+@mod_menuitems.route('/menu/soups-salads')
+def render_menu_soups_salads():
+    # Todo: Select menuitems belonging to category
+    menuitems = Menuitem.query.all()
+    return render_template('pages/menu-soups-salads.html', menuitems = menuitems)
+
+@mod_menuitems.route('/menu/signature-dishes')
+def render_menu_main_dishes():
+    # Todo: Select menuitems belonging to category
+    menuitems = Menuitem.query.all()
+    return render_template('pages/menu-main-dishes.html', menuitems = menuitems)
+
+@mod_menuitems.route('/menu/desserts')
+def render_menu_desserts():
+    # Todo: Select menuitems belonging to category
+    menuitems = Menuitem.query.all()
+    return render_template('pages/menu-desserts.html', menuitems = menuitems)
+
+@mod_menuitems.route('/menu/drinks')
+def render_menu_drinks():
+    # Todo: Select menuitems belonging to category
+    menuitems = Menuitem.query.all()
+    return render_template('pages/menu-drinks.html', menuitems = menuitems)
+
 @mod_menuitems.route('/new')
 def new():
     require_admin()
